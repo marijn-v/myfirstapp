67502a49f02004b166ec7798630faec0e6ffc28b
marijn-v <marijnverbiesen@gmail.com>
2021-09-06 10:35:05 +0200

    my first commit

---
+++ src/Title.js
@@ -0,0 +1,7 @@
+export default function Title(){ // export always, so you can import
+
+    // before return you write all the js (functions, object, variables)
+    return (
+        <div></div>
+    )
+}
